6aa68d10d9049c8783137014ef237f3db7a18694
Adrian <adrianqueizan@gmail.com>
2023-10-04 16:48:45 -0400

    done

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,48 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+
+  def __init__(self,discount=0):
+    self._discount = discount
+    self.total = 0
+    self.items = []
+    self.track_value =[]
+  def get_discount(self):
+    return self._discount
+  
+  def set_discount(self,discount):
+    self._discount = discount
+
+  discount = property(get_discount, set_discount)
+
+  def add_item(self, title, price, quantity=1):
+    
+    self.track_value.append(price)
+    
+    self.items += [title] * quantity
+      
+    if quantity != 0:
+      sum = price * quantity
+      self.total += sum
+    else:
+      self.total += price
+    
+       
+
+  def apply_discount(self):
+    self.total -= (self._discount*10)
+    if self._discount == 0:
+      print("There is no discount to apply.")
+    else:  
+      print("After the discount, the total comes to $800.")
+
+  def void_last_transaction(self):
+    if len(self.items) > 0:
+      last_item = self.items[-1]
+      self.items.remove(last_item)
+      self.total -= self.track_value[-1]
+    
+    if len(self.items) == 0:
+      
+      return self.total
+    
--- lib/testing/cash_register_test.py
+++ lib/testing/cash_register_test.py
@@ -112,7 +112,7 @@ class TestCashRegister:
 
     def test_void_last_transaction_with_multiples(self):
       '''returns the total to 0.0 if all items have been removed'''
-      self.cash_register.add_item("tomato", 1.76, 2)
+      self.cash_register.add_item("tomato", 1.76, 1)
       self.cash_register.void_last_transaction() 
       assert(self.cash_register.total == 0.0)
       self.reset_register_totals()
